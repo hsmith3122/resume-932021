53f5faf83924ce1453b3bff031d15f81381abc10
hsmith3122 <67619336+hsmith3122@users.noreply.github.com>
2021-09-02 03:03:03 -0400

    Added September 3, 2021's updated resume

---
+++ September 3 2021/resume.js
@@ -0,0 +1,19 @@
+var sidebar = document.getElementById("sidebar");
+var main = document.getElementById("main");
+
+setBodyBackground();
+
+function setBodyBackground(){
+    
+    let sidebarColor = window.getComputedStyle(sidebar).backgroundColor;
+    let mainColor = window.getComputedStyle(main).backgroundColor;
+    var sbcols = sidebarColor.substring(4, sidebarColor.length - 1).split(/,\s*/);
+    var mcols = mainColor.substring(4, mainColor.length - 1).split(/,\s*/);
+    var halfcols = [
+        ((parseInt(sbcols[0]) + parseInt(mcols[0])) / 2),
+        ((parseInt(sbcols[1]) + parseInt(mcols[1])) / 2),
+        ((parseInt(sbcols[2]) + parseInt(mcols[2])) / 2),
+    ]
+    document.body.style.backgroundColor = "rgb(" + halfcols[0] + ", " + halfcols[1] + ", " + halfcols[2] + ")";
+
+}
